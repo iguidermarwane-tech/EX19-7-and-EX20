a26d98c03c9bb9aa09c9c78d5aa5ed3c848b77c5
iguidermarwane-tech <iguider.marwane@ensam-casa.ma>
2025-10-31 23:29:14 +0100

    Delete activite_4.py

--- activite_4.py
+++
@@ -1,6 +0,0 @@
-file=open("muliplication.txt","w+")
-for i in range(1,11):
-    file.write(f"tableau de multiplication de {i}: \n")
-    for j in range(1,11):
-        file.write(f"      {i:02d}x{j:02d}={str(i*j)}\n")
-file.close()
